2ac4ac20dd54cd20a4607a349d6a37becd447fd2
MB <sr.funk.sensei@gmail.com>
2019-08-01 12:39:09 +0200

    initial commit

---
+++ src/main/java/com/mb/OgnioConquerGameApplication.java
@@ -0,0 +1,13 @@
+package com.mb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OgnioConquerGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OgnioConquerGameApplication.class, args);
+	}
+
+}
